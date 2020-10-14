acc24e3abc54094e0069ffd39c0dcdbf1657a698
kyuu5988 <kyuu5988@yahoo.co.jp>
2020-10-14 21:27:51 +0900

    item model 仮設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,9 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :ctg, :de_are, :de_day, :de_fee, :status
+
+  validates :p_name, :p_info, :ctg, :de_are, :de_day, :de_fee, :status, presence: true
+
+  #ジャンルの選択が「--」の時は保存できないようにする
+  validates :ctg_id, :de_are_id, :de_day_id, :de_fee_id, :status_id, numericality: { other_than: 1 }
 end
