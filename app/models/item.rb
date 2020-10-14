40e56a1758525fd3c41edf28dc811e54d1e8cf6b
kyuu5988 <kyuu5988@yahoo.co.jp>
2020-10-14 21:40:03 +0900

    active hash form 設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,10 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :ctg, :de_are, :de_day, :de_fee, :status
+  belongs_to_active_hash :ctg
+  belongs_to_active_hash :de_are
+  belongs_to_active_hash :de_fee
+  belongs_to_active_hash :de_day
+  belongs_to_active_hash :status
 
   validates :p_name, :p_info, :ctg, :de_are, :de_day, :de_fee, :status, presence: true
 
