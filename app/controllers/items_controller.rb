f3ace8c44f6a6ebfbbd836777dba772b5d062dad
kyuu5988 <kyuu5988@yahoo.co.jp>
2020-10-14 20:39:57 +0900

    active_hash 追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,15 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new    
   end
+
+  def create
+    @item = Item.new    
+  end
+
+
+  private
+
+  def item_params
+    
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_13_043138) do
+ActiveRecord::Schema.define(version: 2020_10_14_111339) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "p_name", null: false
+    t.text "p_info", null: false
+    t.integer "ctg_id", null: false
+    t.integer "status_id", null: false
+    t.integer "de_fee_id", null: false
+    t.integer "de_are_id", null: false
+    t.integer "de_day_id", null: false
+    t.integer "price", null: false
+    t.boolean "sold", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -30,4 +46,5 @@ ActiveRecord::Schema.define(version: 2020_10_13_043138) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
