0200afec61220561957981509ebbfeed214ef98e
kyuu5988 <kyuu5988@yahoo.co.jp>
2020-10-14 21:04:20 +0900

    formの選択肢設定

--- app/models/ctg.rb
+++ app/models/ctg.rb
@@ -1,2 +1,14 @@
-class Ctg < ApplicationRecord
+class Ctg < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: 'レディーストップス' },
+    { id: 3, name: 'メンズトップス' },
+    { id: 4, name: 'レディースボトムス' },
+    { id: 5, name: 'メンズボトムス' },
+    { id: 6, name: 'ベビー・キッズ' },
+    { id: 7, name: 'ホーム・インテリア' },
+    { id: 8, name: 'アクセサリー・貴金属' },
+    { id: 9, name: '電気製品・スマホ関連' },
+    { id: 10, name: 'その他' }
+  ]
 end
--- app/models/de_day.rb
+++ app/models/de_day.rb
@@ -1,2 +1,8 @@
-class DeDay < ApplicationRecord
+class DeDay < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: '1-2日' },
+    { id: 3, name: '3-5日' },
+    { id: 4, name: '4-7日' },
+  ]
 end
