e0415ff2acedd6b4798bcdb2710518066e7493cf
kyuu5988 <kyuu5988@yahoo.co.jp>
2020-10-15 21:34:39 +0900

    数値バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,4 +12,11 @@ class Item < ApplicationRecord
   validates :p_name, :p_info, :ctg, :de_are, :de_day, :de_fee, :status, presence: true
 
   validates :ctg_id, :de_are_id, :de_day_id, :de_fee_id, :status_id, numericality: { other_than: 1 }
+
+  validates :price, presence: true, 
+            :numericality => {
+              :greater_than_or_equal_to => 300,
+              :less_than_or_equal_to => 9999999,
+              :message => '指定の範囲で金額で入力して下さい。' 
+           }
 end
