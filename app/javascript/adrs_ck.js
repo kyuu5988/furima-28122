672bb6b6b4e505a5a85f45fe0b3a6e068f1c342f
kyuu5988 <kyuu5988@yahoo.co.jp>
2020-10-23 22:47:20 +0900

    郵便・都道府県　ck ok

--- app/javascript/adrs_ck.js
+++ app/javascript/adrs_ck.js
@@ -1,23 +1,36 @@
 const adrs_ck = () => {
   console.log('adrs_ck.js発動中');//動作ck用
 
-    //フォーム入力確認
+    //郵便番号確認
     const post = document.getElementById("postal-code");
     const postOk = document.getElementById("post-ok");
 
     post.addEventListener('input', function(){
-      console.log('郵便チェック')//動作ck
-      if (post.value.match(/^\d{3}-?\d{4}$/g) ) {
+      console.log('郵便ck')//動作ck
+      if (post.value.match(/^[0-9]{3}-[0-9]{4}$/) ) {
         postOk.innerHTML = `OK`
         postOk.setAttribute("style","background-color:green");
       }else{
         postOk.innerHTML = `ハイフンを入れて半角数字で入れて下さい`
         postOk.setAttribute("style","background-color:red");
       }
-
-
     })
 
+    //都道府県選択確認
+    const pref = document.getElementById("prefecture");
+    const prefOk = document.getElementById("pref-ok");
+
+    pref.addEventListener('change',function(){
+      console.log('都道府県選択ck')//動作ck
+      if (pref.select != `--` ){
+        prefOk.innerHTML = `OK`
+        prefOk.setAttribute("style","background-color:green");
+      }
+    })
+
+
+
+    
 
 };
 window.addEventListener("load", adrs_ck);
--- app/models/ord_adrs.rb
+++ app/models/ord_adrs.rb
@@ -4,11 +4,11 @@ class OrdAdrs
 
   with_options presence: true do
     validates :post, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "ハイフンを入れて正しく入力して下さい。"}
-    validates :city, :area, :token, :price
+    validates :city, :area, :token, :price, :pref_id
     validates :phone, format: { with: /\A[0-9]{10,11}\z/, message: "10桁または11桁の正しい番号を入力して下さい。（ハイフン不要）"}
   end
 
-  validates :pref_id, numericality: { other_than: 0, message: "必ず選択して下さい。" }
+  # validates :pref_id, numericality: { other_than: 0, message: "必ず選択して下さい。" }
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)    
