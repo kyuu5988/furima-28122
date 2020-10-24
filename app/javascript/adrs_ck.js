0b2bdd95f513f31e104d644cdfdf2fc9d413de80
kyuu5988 <kyuu5988@yahoo.co.jp>
2020-10-24 09:04:51 +0900

    js による購入ボタン隠し完了

--- app/javascript/adrs_ck.js
+++ app/javascript/adrs_ck.js
@@ -1,6 +1,67 @@
 const adrs_ck = () => {
   console.log('adrs_ck.js発動中');//動作ck用
 
+    //カード番号確認
+    const card = document.getElementById("card-number");
+    const cardOk = document.getElementById("card-ok");
+
+    card.addEventListener('input', function(){
+      console.log('カード番号ck')//動作ck
+      if (card.value.match(/^[0-9]{16}$/) ) {
+        cardOk.innerHTML = `OK`
+        cardOk.setAttribute("style","background-color:green");
+      }else{
+        cardOk.innerHTML = `16桁の半角数字のみで入力して下さい`
+        cardOk.setAttribute("style","background-color:red");
+      }
+    })
+
+    //有効mon確認
+    const expMon = document.getElementById("card-exp-month");
+    const expYear = document.getElementById("card-exp-year");
+    const expOk = document.getElementById("exp-ok");
+
+    expMon.addEventListener('input', function(){
+      console.log('有効monck')//動作ck
+      if (expMon.value.match(/^[0-9]{1,2}$/) 
+        && expYear.value.match(/^[0-9]{1,2}$/) ) {
+        expOk.innerHTML = `OK`
+        expOk.setAttribute("style","background-color:green");
+      }else{
+        expOk.innerHTML = `2桁までの半角数字のみで入力して下さい`
+        expOk.setAttribute("style","background-color:red");
+      }
+    })
+
+    //有効year確認
+    expYear.addEventListener('input', function(){
+      console.log('有効yearck')//動作ck
+      if (expMon.value.match(/^[0-9]{1,2}$/) 
+        && expYear.value.match(/^[0-9]{1,2}$/) ) {
+        expOk.innerHTML = `OK`
+        expOk.setAttribute("style","background-color:green");
+      }else{
+        expOk.innerHTML = `2桁までの半角数字のみで入力して下さい`
+        expOk.setAttribute("style","background-color:red");
+      }
+    })
+
+    //有効cvc確認
+    const cardCvc = document.getElementById("card-cvc");
+    const cvcOk = document.getElementById("cvc-ok");
+
+    cardCvc.addEventListener('input', function(){
+      console.log('cvc-ck')//動作ck
+      if (cardCvc.value.match(/^[0-9]{3,4}$/)) {
+        cvcOk.innerHTML = `OK`
+        cvcOk.setAttribute("style","background-color:green");
+      }else{
+        cvcOk.innerHTML = `3〜4桁までの半角数字のみで入力して下さい`
+        cvcOk.setAttribute("style","background-color:red");
+      }
+    })
+    
+
     //郵便番号確認
     const post = document.getElementById("postal-code");
     const postOk = document.getElementById("post-ok");
@@ -68,7 +129,7 @@ const adrs_ck = () => {
         telOk.innerHTML = `OK`
         telOk.setAttribute("style","background-color:green");
       }else{
-        telOk.innerHTML = `10〜11桁の半角数字で入れて下さい`
+        telOk.innerHTML = `10〜11桁の半角数字のみで入力して下さい`
         telOk.setAttribute("style","background-color:red");
       }
     })
@@ -80,7 +141,8 @@ const adrs_ck = () => {
       console.log('ボタン隠し')//動作ck
       if (postOk.innerHTML == `OK` && prefOk.innerHTML == `OK` 
       && cityOk.innerHTML == `OK` && areaOk.innerHTML == `OK` 
-      && telOk.innerHTML == `OK` ){
+      && telOk.innerHTML == `OK` && cardOk.innerHTML == `OK` 
+      && expOk.innerHTML == `OK` && cvcOk.innerHTML == `OK` ){
         buyBtn.setAttribute("style", "display:block;");
         neIn.setAttribute("style", "display:none;");
       } else {
