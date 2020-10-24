0e1b4d523f29b480a35ef86be9aeecc7bb5e3efa
kyuu5988 <kyuu5988@yahoo.co.jp>
2020-10-24 09:24:24 +0900

    fee.js 常時発動を修正
    入力時のみ反応するようにした

--- app/javascript/fee.js
+++ app/javascript/fee.js
@@ -1,10 +1,18 @@
 function fee() {
-  const inputPrice = document.getElementById("item-price").value;
+  console.log('fee.js発動中');//動作ck用
+
   const selesCommission = document.getElementById("add-tax-price")
   const selesProfit = document.getElementById("profit")
+  const inputCk = document.getElementById("item-price");
 
-    fee10 = inputPrice * 0.1
-    selesCommission.innerHTML = fee10.toLocaleString('ja-JP', { style: 'currency', currency: 'JPY' });
-    selesProfit.innerHTML = (inputPrice - fee10).toLocaleString('ja-JP', { style: 'currency', currency: 'JPY' });
+    inputCk.addEventListener('input', function(){
+      console.log('入力計算発動中');//動作ck用
+
+      const inputPrice = document.getElementById("item-price").value;
+      fee10 = inputPrice * 0.1
+      selesCommission.innerHTML = fee10.toLocaleString('ja-JP', { style: 'currency', currency: 'JPY' });
+      selesProfit.innerHTML = (inputPrice - fee10).toLocaleString('ja-JP', { style: 'currency', currency: 'JPY' });
+    })
+
 }
-setInterval(fee, 1000);
+window.addEventListener("load", fee);
